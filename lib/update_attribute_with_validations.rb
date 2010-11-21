a2806b2b506d727c29989167ced25287db643e9a
Jonathan Garvin <jgarvin@wwidea.org>
2010-11-21 08:50:11 -0700

    fix spelling error

--- lib/update_attribute_with_validations.rb
+++ lib/update_attribute_with_validations.rb
@@ -1,5 +1,5 @@
 module ActiveRecord
-  module Persistance
+  module Persistence
     def update_attribute_with_validations(name, value)
       raise ActiveRecordError, "#{name} is marked as readonly" if self.class.readonly_attributes.include?(name.to_s)
       with_transaction_returning_status do
